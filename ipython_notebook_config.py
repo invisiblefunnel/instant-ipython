1d6d55975e0658f2bb807e4aaef19b133c79b368
Danny Whalen <daniel.r.whalen@gmail.com>
2015-08-19 22:04:50 -0700

    Set default user

--- ipython_notebook_config.py
+++ ipython_notebook_config.py
@@ -9,13 +9,13 @@ c.NotebookApp.contents_manager_class = PostgresContentsManager
 # Set the url for the database used to store files.  See
 # http://docs.sqlalchemy.org/en/rel_0_9/core/engines.html#postgresql
 # for more info on db url formatting.
-c.PostgresContentsManager.db_url = environ['DATABASE_URL']
+c.PostgresContentsManager.db_url = environ.get('DATABASE_URL')
 
 # PGContents associates each running notebook server with a user, allowing
 # multiple users to connect to the same database without trampling each other's
 # notebooks. By default, we use the result of result of getpass.getuser(), but
 # a username can be specified manually like so:
-# c.PostgresContentsManager.user_id = 'my_awesome_username'
+c.PostgresContentsManager.user_id = environ.get('IPYTHON_USER', 'default')
 
 # Set a maximum file size, if desired.
 c.PostgresContentsManager.max_file_size_bytes = 1000000 # 1MB File cap
